401fcd4716a000421b3945bf2f5f53190698f4c8
Javier-Rivera2311 <142764215+Javier-Rivera2311@users.noreply.github.com>
2024-03-04 01:11:11 -0300

    hasta seccion 3

---
+++ 01-tipos/07-arrays.js
@@ -0,0 +1,9 @@
+let animales = ["Perro","Gato","Lobo","León","Tigre","Oso"];
+
+console.log(animales[0]); // Perro
+animales[6] = "Pato";
+console.log(animales);
+
+console.log(typeof animales); // tipo de dato object
+console.log(animales.length); // 7
+
