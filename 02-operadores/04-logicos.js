5c511f55dbf708fa02d6a7a91116d9ea762b71eb
Javier-Rivera2311 <142764215+Javier-Rivera2311@users.noreply.github.com>
2024-03-06 01:30:54 -0300

    seccion 3 completa

---
+++ 02-operadores/04-logicos.js
@@ -0,0 +1,29 @@
+// AND, OR, NOT
+
+//AND &&
+
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(false && false); // false
+
+//OR ||
+
+console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(false || false); // false
+
+//NOT !
+
+console.log(!true); // false
+console.log(!false); // true
+
+// Ejemplo
+
+let a = 10;
+let b = 5;
+
+console.log(a > 5 && b > 5); // false
+
+console.log(a > 5 || b > 5); // true
